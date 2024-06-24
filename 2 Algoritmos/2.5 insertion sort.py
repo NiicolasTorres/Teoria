1a4e45e25166f9d10765e06eba6e6ab1ff0e144c
Federico Torres <federico.-torres@hotmail.com>
2024-06-24 12:07:33 -0300

    se comento el codig para mostrar que hace cada cosa

--- 2 Algoritmos/2.5 insertion sort.py
+++ 2 Algoritmos/2.5 insertion sort.py
@@ -1,13 +1,24 @@
-arr = [5, 3 ,4 ,8 ,7 ,5 ,1 ,2 ,3]
+# Inicializamos una lista de números desordenados
+arr = [5, 3, 4, 8, 7, 5, 1, 2, 3]
+# Imprimimos la lista original
 print(arr)
 
+# Comenzamos un bucle que recorrerá la lista desde el segundo elemento hasta el final
 for j in range(1, len(arr)):
+    # Guardamos el elemento actual en una variable 'actual'
     actual = arr[j]
     
+    # Inicializamos otra variable 'i' con el índice del elemento anterior al actual
     i = j - 1
-    while i >= 0 and arr[i]> actual:
-        arr[i+1] = arr[i]
-        i-=1
-    arr[i+1] = actual
-    
+    # Iniciamos un bucle que se ejecuta mientras 'i' sea mayor o igual a 0
+    # y el elemento en 'arr[i]' sea mayor que 'actual'
+    while i >= 0 and arr[i] > actual:
+        # Desplazamos el elemento 'arr[i]' una posición a la derecha
+        arr[i + 1] = arr[i]
+        # Decrementamos 'i' para comparar el siguiente elemento hacia la izquierda
+        i -= 1
+    # Insertamos el 'actual' en la posición correcta, después de haber desplazado los mayores
+    arr[i + 1] = actual
+
+# Imprimimos la lista ordenada
 print(arr)
